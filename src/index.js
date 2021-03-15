55dec2718364457736c2defb61942a66426af582
Vinel-Ivan <vinel.ivan051@gmail.com>
2021-03-15 13:20:03 +0300

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const buffer = [];
+
+  for (let i=0; i < str.length; i++) {
+    const conf = str[i];
+    const rule1 = bracketsConfig.filter(i => i[0] === conf);
+    const rule2 = bracketsConfig.filter(i => i[1] === conf);
+    const rule3 = (popedItem) => !rule2.some(i => i[0] === popedItem);
+
+    if (rule1.length !== 0 && rule2.length === 0) {
+      stack.push(conf);
+      continue;
+    } else if (rule2.length !== 0 && rule1.length === 0) {
+      const popedItem = stack.pop();
+      if (rule3(popedItem)) {
+        return false;
+      }
+      continue;
+    }
+
+    if(rule1.length !== 0 && buffer[buffer.length - 1] !== conf) {
+      stack.push(conf);
+      buffer.push(conf);
+    } else {
+      stack.pop();
+      buffer.pop();
+    }
+  }
+
+  return stack.length === 0;
 }
+
